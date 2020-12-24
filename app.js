9b0036c12821bf3b98ce50a56d12ee968aae93cd
trianlocvang <phamminhtrung263@gmail.com>
2020-12-24 12:08:27 +0700

    ip

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var DATA_PATH = 'data.json?v=0.3';
-var DEFAULT_SEARCH_TERM = 'Nhập mã nhân viên tại đây';
+var DEFAULT_SEARCH_TERM = 'Nhập số điện thoại tại đây';
 
 var getLangConfig = function () {
   return {
